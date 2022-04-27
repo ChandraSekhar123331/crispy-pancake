0913ca834625d07b536e1fcaf082f6966aff43e2
ChandraSekhar123331 <190050031@iitb.ac.in>
2022-04-27 18:49:05 +0530

    Structure Finalised

---
+++ RMS-App/backend/routes/ingredients.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const ingredCrudController = require('../controllers/ingredients/crud');
+
+const router = express.Router();
+
+router.post('/create', ingredCrudController.insert);
+router.post('/delete', ingredCrudController.dlete);
+router.post('/update', ingredCrudController.update);
+router.get('/getAllInfo', ingredCrudController.getAllInfo);
+router.get('/getOneInfo', ingredCrudController.getOneInfo);
