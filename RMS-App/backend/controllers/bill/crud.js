353d172c224363da69284a7bc0b291fff6e8202e
ChandraSekhar123331 <190050031@iitb.ac.in>
2022-04-29 03:05:47 +0530

    OnlineBill code done ig. Need to test via frontend

--- RMS-App/backend/controllers/bill/crud.js
+++ RMS-App/backend/controllers/bill/crud.js
@@ -1,7 +1,179 @@
-const insert = function insert(req, res) {};
-const getAllInfo = function getAllInfo(req, res) {};
-const getOneInfo = function getOneInfo(req, res) {};
+const freeAttendService = require('../../services/employee/attendant/freeAttendant');
+const billCrudService = require('../../services/bill/crud');
+const orderItemService = require('../../services/bill/orderItem');
 
-exports.insert = insert;
-exports.getAllInfo = getAllInfo;
-exports.getOneInfo = getOneInfo;
+const insertOnlineBill = function insertOnlineBill(req, res) {
+  const customerId = req.session.user.id;
+  const { role, itemList } = req.session.user;
+  if (customerId == null) {
+    return res.status(403).json({
+      message: 'customerId not defined. Unauthorised access.',
+      code: -1,
+      result: null,
+    });
+  }
+  if (role == null) {
+    return res.status(403).json({
+      message: 'role not defined. Unauthorised access.',
+      code: -1,
+      result: null,
+    });
+  }
+  if (itemList == null) {
+    return res.status(403).json({
+      message: "itemList can't be null for an order",
+      code: -1,
+      result: null,
+    });
+  }
+  if (itemList.length === 0) {
+    return res.status(403).json({
+      message: "itemList can't be null for an order",
+      code: -1,
+      result: null,
+    });
+  }
+  const orderType = 'online';
+  // need to check if role is customer or not.
+
+  return freeAttendService
+    .getFreeOnlineAttendant()
+    .then((attendantResponse) => {
+      if (attendantResponse.result.rowCount === 0) {
+        return res.status(403).json({
+          message: 'No attendant free. Please reorder later.',
+          code: -1,
+          result: null,
+        });
+      }
+      const attendantId = attendantResponse.result.rows[0].attendant_id;
+      // create a bill
+      return billCrudService
+        .insert(customerId, orderType)
+        .then((billResponse) => {
+          const billId = billResponse.result.rows[0].bill_id;
+          // assign attendant
+          return freeAttendService
+            .assignAttendant(billId, attendantId)
+            .then(() => {
+              // insert items.
+              for (let i = 0; i < itemList.length; i += 1) {
+                orderItemService
+                  .insert(billId, itemList[i].dishId, itemList[i].quantity)
+                  .catch((error) =>
+                    res.status(409).json({
+                      message: error.message,
+                      code: -1,
+                      result: null,
+                    }),
+                  );
+              }
+              return res.status(200).json({
+                message: 'Success',
+                code: 0,
+                result: billResponse.result.rows[0],
+              });
+            })
+            .catch((error) =>
+              res.status(409).json({
+                message: error.message,
+                code: -1,
+                result: null,
+              }),
+            );
+          // assign items.
+        })
+        .catch((error) =>
+          res.status(409).json({
+            message: error.message,
+            code: -1,
+            result: null,
+          }),
+        );
+    })
+    .catch((error) =>
+      res.status(409).json({
+        message: error.message,
+        code: -1,
+        result: null,
+      }),
+    );
+};
+// const getAllInfo = function getAllInfo(req, res) {
+//   const { skip, lim } = req.query;
+//   if (skip == null || lim == null) {
+//     return res.status(409).json({
+//       message: "skip, lim can't be null",
+//       code: -1,
+//       result: null,
+//     });
+//   }
+//   if (skip < 0) {
+//     return res.status(409).json({
+//       message: 'skip should be non-negative',
+//       code: -1,
+//       result: null,
+//     });
+//   }
+//   if (lim < 0) {
+//     return res.status(409).json({
+//       message: 'lim should be non-negative',
+//       code: -1,
+//       result: null,
+//     });
+//   }
+//   return billCrudService
+//     .getAllInfo(skip, lim)
+//     .then((response) =>
+//       res.status(200).json({
+//         message: 'Success',
+//         code: 0,
+//         result: response.result.rows,
+//       }),
+//     )
+//     .catch((error) =>
+//       res.status(409).json({
+//         message: error.message,
+//         code: -1,
+//         result: null,
+//       }),
+//     );
+// };
+// const getOneInfo = function getOneInfo(req, res) {
+//   const { billId } = req.query;
+//   if (billId == null) {
+//     return res.status(409).json({
+//       message: 'billId should not be null',
+//       code: -1,
+//       result: null,
+//     });
+//   }
+//   return billCrudService
+//     .getOneInfo(billId)
+//     .then((response) => {
+//       if (response.result.rowCount === 0) {
+//         return res.status(409).json({
+//           message: 'stockId not found in the Database',
+//           code: -1,
+//           result: null,
+//         });
+//       }
+//       return res.status(200).json({
+//         message: 'Success',
+//         code: 0,
+//         result: response.result.rows[0],
+//       });
+//     })
+//     .catch((error) =>
+//       res.status(409).json({
+//         message: error.message,
+//         code: -1,
+//         result: null,
+//       }),
+//     );
+// };
+
+exports.insertOnlineBill = insertOnlineBill;
+// exports.insertOfflineBill = insertOfflineBill;
+// exports.getAllInfo = getAllInfo;
+// exports.getOneInfo = getOneInfo;
