0914556c5a65f90591da5edfd0621598fd74ef13
ChandraSekhar123331 <190050031@iitb.ac.in>
2022-04-28 15:07:09 +0530

    crud customer done. update, delete not done

--- RMS-App/backend/controllers/customer/crud.js
+++ RMS-App/backend/controllers/customer/crud.js
@@ -1,38 +1,154 @@
-// const bcrypt = require('bcryptjs');
-// const registerService = require('../../services/register');
+const bcrypt = require('bcryptjs');
+const customerCrudService = require('../../services/customer/crud');
 
 const insert = function insert(req, res) {
-  // const { firstname, surname, email, password } = req.query;
-  // if (
-  //   firstname == null ||
-  //   surname == null ||
-  //   email == null ||
-  //   password == null
-  // ) {
-  //   return res.sendStatus(403);
-  // }
-  // const hashedPassword = bcrypt.hashSync(password, 10);
-  // return registerService
-  //   .registerUser(firstname, surname, email, hashedPassword)
-  //   .then((response) => {
-  //     const user = response;
-  //     req.session.user = {
-  //       id: user.id,
-  //       firstname: user.firstname,
-  //       surname: user.surname,
-  //       email: user.email,
-  //     };
-  //     res.status(200);
-  //     return res.json({ user: req.session.user });
-  //   })
-  //   .catch(() => {
-  //     res.status(403);
-  //     return res.json({ message: 'Unable to insert the user' });
-  //   });
+  const { userName, fullName, emailId, phoneNumber, address, password } =
+    req.query;
+
+  if (userName == null) {
+    return res.status(409).json({
+      message: "userName can't be null",
+      code: -1,
+      result: null,
+    });
+  }
+  if (fullName == null) {
+    return res.status(409).json({
+      message: "fullName can't be null",
+      code: -1,
+      result: null,
+    });
+  }
+  if (emailId == null) {
+    return res.status(409).json({
+      message: "emailId can't be null",
+      code: -1,
+      result: null,
+    });
+  }
+  if (phoneNumber == null) {
+    return res.status(409).json({
+      message: "phoneNumber can't be null",
+      code: -1,
+      result: null,
+    });
+  }
+  if (address == null) {
+    return res.status(409).json({
+      message: "address can't be null",
+      code: -1,
+      result: null,
+    });
+  }
+  if (password == null) {
+    return res.status(409).json({
+      message: "password can't be null",
+      code: -1,
+      result: null,
+    });
+  }
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  return customerCrudService
+    .insert(userName, fullName, emailId, phoneNumber, address, hashedPassword)
+    .then((response) => {
+      const user = response.result.rows[0];
+      req.session.user = {
+        id: user.customer_id,
+        userName: user.user_name,
+        fullName: user.full_name,
+        emailId: user.email_id,
+        address: user.cust_address,
+        role: 'Customer',
+      };
+      return res.status(200).json({
+        message: 'Success',
+        code: 0,
+        result: req.session.user,
+      });
+    })
+    .catch((error) =>
+      res.status(409).json({
+        message: error.message,
+        code: -1,
+        result: null,
+      }),
+    );
 };
+const getAllInfo = function getAllInfo(req, res) {
+  const { skip, lim } = req.query;
+  if (skip == null || lim == null) {
+    return res.status(409).json({
+      message: "skip, lim can't be null",
+      code: -1,
+      result: null,
+    });
+  }
+  if (skip < 0) {
+    return res.status(409).json({
+      message: 'skip should be non-negative',
+      code: -1,
+      result: null,
+    });
+  }
+  if (lim < 0) {
+    return res.status(409).json({
+      message: 'lim should be non-negative',
+      code: -1,
+      result: null,
+    });
+  }
+  return customerCrudService
+    .getAllInfo(skip, lim)
+    .then((response) =>
+      res.status(200).json({
+        message: 'Success',
+        code: 0,
+        result: response.result.rows,
+      }),
+    )
+    .catch((error) =>
+      res.status(409).json({
+        message: error.message,
+        code: -1,
+        result: null,
+      }),
+    );
+};
+const getOneInfo = function getOneInfo(req, res) {
+  const { customerId } = req.query;
+  if (customerId == null) {
+    return res.status(409).json({
+      message: "customerId can't be null",
+      code: -1,
+      result: null,
+    });
+  }
+  return customerCrudService
+    .getOneInfo(customerId)
+    .then((response) => {
+      if (response.result.rowCount === 0) {
+        return res.status(409).json({
+          message: 'customerId not found in the Database',
+          code: -1,
+          result: null,
+        });
+      }
+      return res.status(200).json({
+        message: 'Success',
+        code: 0,
+        result: response.result.rows[0],
+      });
+    })
+    .catch((error) =>
+      res.status(409).json({
+        message: error.message,
+        code: -1,
+        result: null,
+      }),
+    );
+};
+
 const update = function update(req, res) {};
-const getAllInfo = function getAllInfo(req, res) {};
-const getOneInfo = function getOneInfo(req, res) {};
 
 exports.insert = insert;
 exports.update = update;
--- RMS-App/backend/services/customer/crud.js
+++ RMS-App/backend/services/customer/crud.js
@@ -1,20 +1,68 @@
-// const { poolObj } = require('./connectDb');
+const { poolObj } = require('../connectDb');
 
-// const registerUser = function registerUser(
-//   firstname,
-//   surname,
-//   email,
-//   password,
-// ) {
-//   const query = `INSERT INTO users(firstname, surname, email, password)
-//   VALUES ($1, $2, $3, $4) RETURNING *`;
-//   return poolObj
-//     .query(query, [firstname, surname, email, password])
-//     .then((response) => Promise.resolve(response.rows[0]))
-//     .catch((error) => {
-//       console.log('Unable to register the user');
-//       return Promise.reject(error);
-//     });
-// };
+const insert = function insert(
+  userName,
+  fullName,
+  emailId,
+  phoneNumber,
+  address,
+  password,
+) {
+  const query = `insert into customer(user_name, full_name, email_id, 
+    phone_number, cust_address, cust_password)
+    values($1, $2, $3, $4, $5, $6)
+    returning *`;
 
-// exports.registerUser = registerUser;
+  return poolObj
+    .query(query, [userName, fullName, emailId, phoneNumber, address, password])
+    .then((response) =>
+      Promise.resolve({
+        message: 'Success',
+        code: 0,
+        result: response,
+      }),
+    )
+    .catch((error) => Promise.reject(new Error(error.message)));
+};
+
+const getOneInfo = function getOneInfo(customerId) {
+  const query = `select user_name, full_name, email_id, cust_address
+  from customer
+  where customer_id = $1`;
+  return poolObj
+    .query(query, [customerId])
+    .then((response) =>
+      Promise.resolve({
+        message: 'Success',
+        code: 0,
+        result: response,
+      }),
+    )
+    .catch((error) => Promise.reject(new Error(error.message)));
+};
+
+const getAllInfo = function getAllInfo(skip, lim) {
+  const query = `select user_name, full_name, email_id, cust_address
+  from customer
+  limit $1
+  offset $2`;
+  return poolObj
+    .query(query, [lim, skip])
+    .then((response) =>
+      Promise.resolve({
+        message: 'Success',
+        code: 0,
+        result: response,
+      }),
+    )
+    .catch((error) => Promise.reject(new Error(error.message)));
+};
+
+const update = function update() {};
+const dlete = function dlete() {};
+
+exports.insert = insert;
+exports.getOneInfo = getOneInfo;
+exports.getAllInfo = getAllInfo;
+exports.update = update;
+exports.dlete = dlete;
