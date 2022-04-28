b0128ee15db4952c4ed8c48ca3e362a4a2c394c0
ChandraSekhar123331 <190050031@iitb.ac.in>
2022-04-28 16:18:32 +0530

    generic employee functions almost done.

--- RMS-App/backend/services/employee/generic.js
+++ RMS-App/backend/services/employee/generic.js
@@ -1,4 +1,57 @@
-// const { poolObj } = require('../connectDb');
+const { poolObj } = require('../connectDb');
+
+const getUser = function getUser(emailId) {
+  const query = `select emp_id, user_name, emp_name, email_id,
+  emp_address, emp_password, emp_role
+  from employee
+  where email_id = $1`;
+
+  return poolObj
+    .query(query, [emailId])
+    .then((response) =>
+      Promise.resolve({
+        message: 'Success',
+        code: 0,
+        result: response,
+      }),
+    )
+    .catch((error) => Promise.reject(new Error(error.message)));
+};
+
+const getOneInfo = function getOneInfo(employeeId) {
+  const query = `select user_name, emp_name, email_id, emp_address
+  from employee
+  where emp_id = $1`;
+  return poolObj
+    .query(query, [employeeId])
+    .then((response) =>
+      Promise.resolve({
+        message: 'Success',
+        code: 0,
+        result: response,
+      }),
+    )
+    .catch((error) => Promise.reject(new Error(error.message)));
+};
+
+const getAllInfo = function getAllInfo(skip, lim) {
+  const query = `select user_name, emp_name, email_id, emp_address
+  from employee
+  limit $1
+  offset $2`;
+  return poolObj
+    .query(query, [lim, skip])
+    .then((response) =>
+      Promise.resolve({
+        message: 'Success',
+        code: 0,
+        result: response,
+      }),
+    )
+    .catch((error) => Promise.reject(new Error(error.message)));
+};
+
+const update = function update() {};
 
 // const insertEmployee = function insertEmployee(
 //   userName,
@@ -43,6 +96,9 @@
 //     );
 // };
 
-// const u
-
 // exports.insert = insertEmployee;
+
+exports.getUser = getUser;
+exports.getOneInfo = getOneInfo;
+exports.getAllInfo = getAllInfo;
+exports.update = update;
