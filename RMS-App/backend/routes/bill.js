1bc0b7a065cb9ff956d9d5f171ad948a9df52517
ChandraSekhar123331 <190050031@iitb.ac.in>
2022-04-29 02:46:11 +0530

    removed unimplemeted routes for now.

--- RMS-App/backend/routes/bill.js
+++ RMS-App/backend/routes/bill.js
@@ -3,9 +3,9 @@ const billCrudController = require('../controllers/bill/crud');
 
 const router = express.Router();
 
-router.post('/create', billCrudController.insert);
-router.get('/getAllinfo', billCrudController.getAllInfo);
-router.get('/getOneinfo', billCrudController.getOneInfo);
+router.post('/onlineBill', billCrudController.insertOnlineBill);
+// router.get('/getAllinfo', billCrudController.getAllInfo);
+// router.get('/getOneinfo', billCrudController.getOneInfo);
 
 // There is really no need to update or delete a bill.
 // Changing a bill is illegal :)
--- RMS-App/backend/routes/table.js
+++ RMS-App/backend/routes/table.js
@@ -6,7 +6,7 @@ const crudController = require('../controllers/table/crud');
 const tableController = require('../controllers/table/table');
 
 router.post('/insert', crudController.insert);
-router.post('/delete', crudController.dlete);
+// router.post('/delete', crudController.dlete);
 router.post('/update', crudController.update);
 router.get('/getAllInfo', crudController.getAllInfo);
 router.get('/getOneInfo', crudController.getOneInfo);
